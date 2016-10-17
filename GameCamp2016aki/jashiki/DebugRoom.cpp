34f8b9b57bad48a2e4d816d3d30d3e18c67d24b9
muneto.jashiki <jashikim17@gmail.com>
2016-10-17 17:23:09 +0900

    createfolder

---
+++ GameCamp2016aki/jashiki/DebugRoom.cpp
@@ -0,0 +1,13 @@
+#include "DebugRoom.h"
+#include "GroundClass.h"
+#include "SceneManagerClass.h"
+
+SceneManagerClass *manag = SingletonClass<SceneManagerClass>::GetInstance();
+
+void DebugInit()
+{
+}
+
+void DebugView()
+{
+}
